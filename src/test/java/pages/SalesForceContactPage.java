115147838f5364ec9089dc1ebf7f1f4a2be8c6d6
marciaDelCastillo <marcia990_14@hotmail.com>
2022-03-07 14:21:25 -0300

    casos 2 a 6 completos

--- src/test/java/pages/SalesForceContactPage.java
+++ src/test/java/pages/SalesForceContactPage.java
@@ -1,11 +1,15 @@
 package pages;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+
+import helpers.Helpers;
 
 public class SalesForceContactPage {
 	private WebDriver driver;
@@ -16,6 +20,7 @@ public class SalesForceContactPage {
 	private By phoneField;
 	private By submitBtn;
 	private By successDiv;
+	private By errorLabels;
 	
 	public SalesForceContactPage(WebDriver driver) {
 		this.driver = driver;
@@ -26,20 +31,65 @@ public class SalesForceContactPage {
 		phoneField= By.id("contact_form_telefono");
 		submitBtn= By.id("submit-landing-form");
 		successDiv = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]");
+		errorLabels = By.tagName("label");
 	}
 
 	public void llenarFormulario(String nombre, String email, String empresa, String producto, String telefono) {
+
+		//cargo los valores
 		driver.findElement(nameField).sendKeys(nombre);
 		driver.findElement(mailField).sendKeys(email);
 		driver.findElement(companyField).sendKeys(empresa);
 		Select selectProduct = new Select(driver.findElement(productDrop));
 		selectProduct.selectByVisibleText(producto);
 		driver.findElement(phoneField).sendKeys(telefono);
+		Helpers helper = new Helpers();
+		helper.sleepSeconds(2);
+		
 		driver.findElement(submitBtn).click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
-	public void assertPage() {
-		Assert.assertTrue(driver.findElement(successDiv).getText().contains("Gracias por contactarte con nosotros"));
+	
+	
+	//datos: nombre, mail, empresa, producto, telefono (index es el valor que viene vacio y cuya validacion debe aparecer)
+	public void assertPageCase2to6(int index) {
+		WebElement labelError=null;
+		String nombreError="";
+		switch(index)
+		{
+		   case 0 :
+		      labelError = driver.findElement(By.id("contact_form_nombre-error"));
+		      nombreError = "Nombre";
+		      break; // break es opcional
+		   case 1 :
+			   labelError = driver.findElement(By.id("contact_form_email-error"));
+			      nombreError = "Email";
+		      break; // break es opcional
+		   case 2 :
+			   labelError = driver.findElement(By.id("contact_form_empresa-error"));
+			      nombreError = "Empresa";
+			  break; // break es opcional   
+		   case 3 :
+			   labelError = driver.findElement(By.id("contact_form_producto-error"));
+			      nombreError = "Producto";
+			  break; // break es opcional
+		   case 4 :
+			   labelError = driver.findElement(By.id("contact_form_telefono-error"));
+			      nombreError = "Teléfono";
+			  break; // break es opcional
+		   default : 
+		      Assert.assertTrue(false, "Error de programación");
+		}
+		Assert.assertTrue(labelError.getText().contains("Este campo es obligatorio"), "La validación del campo "+nombreError+" no funcionó");
+		/*
+		List <WebElement> mensajes = driver.findElements(errorLabels);
+		Assert.assertTrue((mensajes.get(0)).getText().contains("Este campo es obligatorio"), "La validación del indice "+index+" no funcionó");
+		*/
+	}
+	
+	public void assertPageCase7() {
+			Assert.assertTrue(driver.findElement(successDiv).getText().contains("Gracias por contactarte con nosotros"), "El formulario no fue enviado");
+		
 	}
 }
--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -22,25 +22,57 @@ public class Tests {
 		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.navigate().to("https://virtualdreams.io/");
+		driver.navigate().to("https://virtualdreams.io/salesforce/landing-salesforce/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-	}
-	@Test
-	public void validacionesCampoRequerido() {
+		/*
 		HomePage homePage = new HomePage(driver);
 		homePage.ingresarASalesForce();
 		Helpers helper = new Helpers();
 		helper.sleepSeconds(2);
 		SalesForcePage salesForcePage = new SalesForcePage(driver);
 		salesForcePage.ingresarAlFormulario();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		*/
+		
+	}
+
+	@Test // Case 2 to 6. resultado esperado: validacion de cada campo que imposibilite el envio del formulario
+	public void llenarFormularioFaltandoUnCampo() {
+		
+		SalesForceContactPage contactPage = new SalesForceContactPage(driver);
+		Helpers helper = new Helpers();
+		//for para correr los distintos casos, en que vaya faltando solo un campo
+		String [] datos= {"marcia", "marcia990_14@hotmail.com", "Delphos", "Marketing", "1122334455"};
+		for(int i=0;i<datos.length;i++) {
+				String [] datosAux={"marcia", "marcia990_14@hotmail.com", "Delphos", "Marketing", "1122334455"};
+				if(i==3) {
+					datosAux[i]="Producto";
+				}else {
+					datosAux[i]="";
+				}
+				driver.navigate().to(driver.getCurrentUrl()); //recargo la pagina
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				contactPage.llenarFormulario(datosAux[0],datosAux[1],datosAux[2],datosAux[3],datosAux[4]);
+				helper.sleepSeconds(2);
+				contactPage.assertPageCase2to6(i);
+		}	
+	}
+
+	
+	@Test // Case 7. resultado esperado: envio correcto del formulario
+	public void llenarFormularioCompletoCorrecto() {
+		
 		SalesForceContactPage contactPage = new SalesForceContactPage(driver);
 		//lectura de casos de prueba
 		//for para correr los distintos casos
 		contactPage.llenarFormulario("marcia", "marcia990_14@hotmail.com", "Delphos", "Marketing", "1122334455");
+		Helpers helper = new Helpers();
 		helper.sleepSeconds(3);
-		contactPage.assertPage();
+		contactPage.assertPageCase7();
 	}
+	
+	
 	@AfterMethod
 	public void tearDown() {
 		driver.close();
